d7ef604ebec08a1e91eb82dafc270bd130b736aa
ashishgarg880 <ashishgarg880@gmail.com>
2021-11-25 16:57:09 +0530

    ss

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,12 @@
 
 function updateStructure(rec1,rec2){
 	//write your code
+	rec1 = {
+		top:'25px',left:'96px',width:'64px',height:'96px',children:[]
+	}
+	rec2= {
+		top:'25px',left:'96px',width:'64px',height:'96px',children:[]
+	}
 }
 
 module.exports = updateStructure;
