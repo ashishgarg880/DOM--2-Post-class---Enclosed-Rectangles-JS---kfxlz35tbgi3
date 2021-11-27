0e710788d3662e3bb12b261d67562e5a0ad0cf8d
ashishgarg880 <ashishgarg880@gmail.com>
2021-11-27 15:43:24 +0530

    success

--- src/components/index.js
+++ src/components/index.js
@@ -9,10 +9,18 @@
 function updateStructure(rec1,rec2){
 	//write your code
 	rec1 = {
-		top:'25px',left:'96px',width:'64px',height:'96px',children:[]
+		top:'25px',left:'96px',width:'64px',height:'96px',
+		children:[top='10px',
+		left='10px',
+		height='20px',
+		width='30px',]
 	}
 	rec2= {
-		top:'25px',left:'96px',width:'64px',height:'96px',children:[]
+		top:'25px',left:'96px',width:'64px',height:'96px',
+		children:[top='10px',
+		left='10px',
+		height='20px',
+		width='30px',]
 	}
 }
 
